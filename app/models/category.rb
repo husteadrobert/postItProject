681acd7286ecaae79bffad0a86488ea53fd128a6
Robert Hustead <husteadrobert@gmail.com>
2017-11-22 21:50:31 +0900

    update slugging

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,10 +7,34 @@ class Category < ActiveRecord::Base
   validates :name, presence: true
 
   def generate_slug
-    self.slug = self.name.gsub(' ', '-')
+    the_slug = create_slug(self.name)
+    post = Post.find_by(slug: the_slug)
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by(slug: the_slug)
+      count += 1
+    end
+    self.slug = the_slug
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def create_slug(name)
+    str = name.strip
+    str.gsub!(/\s*[^A-Za-z0-9]\s*/, '-')
+    str.gsub!(/-+/, '-')
+    str.downcase
   end
 
   def to_param
     self.slug
   end
+  
 end
